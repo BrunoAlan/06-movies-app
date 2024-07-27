2c5d102b5fd14c73abd3a022234b5fed4943103a
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 12:57:17 -0300

    feat: Add lazy loading for movie images in HorizontalCarousel component

--- src/presentation/components/movies/HorizontalCarousel.tsx
+++ src/presentation/components/movies/HorizontalCarousel.tsx
@@ -1,13 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import type { Movie } from '../../../core/entities/movie.entity';
 import { FlatList } from 'react-native-gesture-handler';
 import MoviePoster from './MoviePoster';
+import { useEffect, useRef } from 'react';
 
 interface Props {
   movies: Movie[];
   title?: string;
+  loadNextPage?: () => void;
 }
-const HorizontalCarousel = ({ movies, title }: Props) => {
+const HorizontalCarousel = ({ movies, title, loadNextPage }: Props) => {
+  const isLoading = useRef(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      isLoading.current = false;
+    }, 200);
+  }, [movies]);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+    if (!isEndReached) {
+      return;
+    }
+    isLoading.current = true;
+    loadNextPage && loadNextPage();
+    // Fetch more movies
+  };
   return (
     <View style={{ height: title ? 260 : 220 }}>
       {title && (
@@ -24,12 +52,13 @@ const HorizontalCarousel = ({ movies, title }: Props) => {
       )}
       <FlatList
         data={movies}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item, index) => `${item.id.toString()}-${index}`}
         renderItem={({ item }) => (
           <MoviePoster movie={item} width={140} height={200} />
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
+        onScroll={onScroll}
       />
     </View>
   );
