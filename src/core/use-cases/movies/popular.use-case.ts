8815757798761187379eb861af585a5baa423674
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 12:57:05 -0300

    feat: Add lazy loading for movie images in popular use case

--- src/core/use-cases/movies/popular.use-case.ts
+++ src/core/use-cases/movies/popular.use-case.ts
@@ -1,15 +1,23 @@
 import { HttpAdapter } from '../../../config/adapters/http/http.adapter';
 import { MovieDBResponse } from '../../../infraestructure/interfaces/movies-db.responses';
 import { MovieMapper } from '../../../infraestructure/mappers.movie.mapper';
-
-// import { MovieMapper } from '../../../infrastructure/mappers/movie.mapper';
 import type { Movie } from '../../entities/movie.entity';
 
+interface Options {
+  page?: number;
+  limit?: number;
+}
+
 export const moviesPopularUseCase = async (
-  fetcher: HttpAdapter
+  fetcher: HttpAdapter,
+  options?: Options
 ): Promise<Movie[]> => {
   try {
-    const popular = await fetcher.get<MovieDBResponse>('/popular');
+    const popular = await fetcher.get<MovieDBResponse>('/popular', {
+      params: {
+        page: options?.page || 1,
+      },
+    });
     return popular.results.map(MovieMapper.fromMovieDBResultToEntity);
   } catch (error) {
     console.log(error);
