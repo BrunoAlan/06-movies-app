ce606e40a92b79438b1f74af9f3eba18c3bfcd09
BrunoAlan <alan.brunoab@gmail.com>
2024-07-27 15:29:06 -0300

    refactor: Update THE_MOVIE_DB_KEY to EXPO_PUBLIC_THE_MOVIE_DB_KEY

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -3,7 +3,7 @@ import { AxiosAdapter } from './http/axios.adapter';
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: 'f6d0014f7b45b2349ea2401483d7ddd0',
+    api_key: process.env.EXPO_PUBLIC_THE_MOVIE_DB_KEY!,
     language: 'en',
   },
 });
